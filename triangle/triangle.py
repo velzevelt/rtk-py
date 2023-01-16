ffdee015ace1d4d74fd5dfde913f3ca0c231afc3
velz <velz@velz>
2023-01-16 11:25:54 +0300

    upd

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,6 +1,7 @@
 from triangle_exceptions import *
 from enum import Enum
 
+
 class TriangleTypes(Enum):
     ISOSCELES = "Равнобедренный"
     EQUILATERAL = "Равносторонний"
@@ -11,19 +12,25 @@ def get_triangle_type(a: int, b: int, c: int) -> str:
     result = 'Неизвестный треугольник'
 
     if a <= 0 or b <= 0 or c <= 0:
-        raise NonExistingTriangleError("Треугольника с отрицательными или нулевыми сторонами не существует")
+        raise NonExistingTriangleError(
+            "Треугольника с отрицательными или нулевыми сторонами не существует")
 
     if (a < b + c) and (b < a + c) and (c < a + b):
-        pass
+        if a == b == c:
+            result = TriangleTypes.EQUILATERAL.value
+        elif a == b or a == c or b == c:
+            result = TriangleTypes.ISOSCELES.value
+        else:
+            result = TriangleTypes.VERSATILE.value
     else:
         raise NonExistingTriangleError
-            
 
     return result
 
 
 def main():
-    print(get_triangle_type(-2, 1, 1))
+    sides = input("Введите стороны треугольника: ")
+    print(get_triangle_type(4, 4, 4))
 
 
 if __name__ == "__main__":
