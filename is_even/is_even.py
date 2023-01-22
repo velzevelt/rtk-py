109806701b48d0af8c621aacfc9418c7b5401bc3
velz <velz@velz>
2023-01-22 16:38:57 +0300

    upd

---
+++ is_even/is_even.py
@@ -0,0 +1,34 @@
+def my_sum(n: int) -> int:
+    n = abs(n)
+
+    if n == 0:
+        return n
+    else:
+        return n + my_sum(n - 1)
+
+
+def is_even(n: int) -> bool:
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def main():
+    try:
+        x = int(input("Введите число "))
+        sum = my_sum(x)
+        if is_even(sum):
+            print(f'{x}, {sum}, "Четно"')
+        else:
+            print(f'{x}, {sum}, "Нечетно"')
+    except ValueError:
+        print("Число не распознано")
+        main()
+    except Exception:
+        print("Неизвестная ошибка")
+        main()
+
+
+if __name__ == "__main__":
+    main()
--- quad_equation/quad_equation.py
+++ quad_equation/quad_equation.py
@@ -1,36 +1,26 @@
 from math import sqrt
-'''
-function solve_equation(int $a = 1, int $b = 1, int $c = 1): array
-{
-    $d = $b ** 2 - 4 * $a * $c;
-    if ($d <= 0)
-    {
-        return ($d == 0 and $a != 0) ? [ -$b / 2 * $a ] : [];
-    }
-    else
-    {
-        $d_root = sqrt($d);
-        $t = 2 * $a;
-        return [ (-$b + $d_root) / $t, (-$b - $d_root) / $t ];
-    }
-}
-
-var_dump( solve_equation(4, -6, 0) );
-
-'''
+from warnings import warn
 
 
 def solve_equation(a: int = 1, b: int = 1, c: int = 1):
     d = b ** 2 - 4 * a * c
+
+    if any(i == 0 for i in (a, b, c)):
+        warn(f"Incorrect input: {a, b, c} Zero forbidden")
+        return False
+
     if d <= 0:
-        if d == 0 and a != 0:
+        if d == 0:
             return -b / 2 * a
         else:
             return False
     else:
         d_root = sqrt(d)
         double_a = 2 * a
-        return [ 
+        return [
             (-b + d_root) / double_a,
-            (-b - d_root) / double_a 
+            (-b - d_root) / double_a
         ]
+
+
+print(solve_equation(10, 5, -26))
