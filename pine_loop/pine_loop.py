7fc8c5785aea6be9e1fcdd2b5ae9b5a436611c7b
Class_Student <Class_Student@rtk.local>
2023-01-25 12:26:53 +0300

    upd

--- pine_loop/pine_loop.py
+++ pine_loop/pine_loop.py
@@ -1,22 +1,39 @@
-def draw_pine(char: str, height: int) -> str:
+def draw_pine(char: str, height: int, outline_only: bool = False) -> str:
     if not (isinstance(char, str) or isinstance(height, int)):
         from warnings import warn
         warn("Некорректный тип входных данных")
         return False
 
     res = ''
-    for i in range(0, height):
+    for i in range(1, height + 1):
         space = " " * (height - i)
-        pine = char * i
-        res += space + pine + pine + "\n"
+        pine_len = i - 1
+
+        if outline_only:
+            if pine_len + 1 == height:
+                res += space + char * i + char * (i - 1)
+            elif pine_len != 0:
+                res += space + char + (" " * pine_len) + (" " * (pine_len - 1) ) + char
+            else:
+                res += space + char + (" " * pine_len)
+        else:
+            if pine_len != 0:
+                res += space + char * i + char * (i - 1)
+            else:
+                res += space + char * i
+
+        res += "\n"
+
     return res
 
 
 def main():
     try:
-        draw_char = str(input("Введите символ елки: "))
-        height = int(input("Введите высоту елки: "))
-        print(draw_pine(draw_char, height))
+        # draw_char = str(input("Введите символ елки: "))
+        # height = int(input("Введите высоту елки: "))
+        # print(draw_pine("8", 16, outline_only=False))
+        print(draw_pine("8", 16, outline_only=True))
+
     except ValueError:
         main()
     except NameError:
