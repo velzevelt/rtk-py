3f1013bdf9ff6923b08d475fe9f70cdc38d5a7ba
velz <velz@velz>
2023-01-25 18:47:26 +0300

    upd

--- sheep_wolf_cabbage_puzzle/puzzle.py
+++ sheep_wolf_cabbage_puzzle/puzzle.py
@@ -16,33 +16,55 @@ def main():
     def get_names(from_side):
         return {actor.nickname: actor for actor in from_side}
 
-    def ship_entity(entity: object, side: list, destination: list) -> None:
+    def sail_entity(entity: object, side: list, destination: list) -> None:
         if entity in side and entity not in destination:
             side.remove(entity)
             destination.append(entity)
 
-    def has_loosed():
-        for actor in finish_side:
-            if actor.goal in finish_side:
-                return f"{actor.nickname} съел {actor.goal.nickname}"
+    def has_loosed(side):
+        for actor in side:
+            if not hasattr(actor, "goal"):
+                continue
+            if actor.goal in side:
+                print(f"{actor.nickname} съел {actor.goal.nickname}")
+                print("Игра завершена")
+                return True
+                
         return False
 
+    def has_won():
+        return len(finish_side) == 3
+
     def make_move(first_side, second_side) -> None:
-        print(f"На изначальном берегу: {first_side.keys()}")
-        print(f"На том берегу: {second_side.keys()}")
+        name_1 = ", ".join(get_names(first_side).keys())
+        name_2 = ", ".join(get_names(second_side).keys())
+        print(f"На этом берегу: {name_1}")
+        print(f"На том берегу: {name_2}")
 
         transfer = input("Кого переправить на тот берег? ")
-        if transfer in first_side:
-            transfer = first_side[transfer]
-            ship_entity(transfer, first_side, second_side)
+        if transfer in get_names(first_side):
+            transfer = get_names(first_side)[transfer]
+            sail_entity(transfer, first_side, second_side)
 
-    while not has_loosed():
-        make_move()
-        loosed = has_loosed()
-        if loosed:
-            print(loosed)
-            print('Игра завершена')
+    while True:
+        make_move(start_side, finish_side)
+        if has_loosed(start_side):
             break
+        if has_won():
+            print("Вы решили задачу")
+            print("Игра завершена")
+            break
+
+        make_move(finish_side, start_side)
+        if has_loosed(finish_side):
+            break
+        if has_won():
+            print("Вы решили задачу")
+            print("Игра завершена")
+            break
+
+    
+
 
 
 main()
