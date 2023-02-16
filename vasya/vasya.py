d066dde25f243904a4d15ca5cd0f12625190b937
Class_Student <Class_Student@rtk.local>
2023-02-16 11:41:17 +0300

    upd

--- vasya/vasya.py
+++ vasya/vasya.py
@@ -12,29 +12,66 @@ def add_one_to_end(num):
     return num * 10 + 1
 
 
-a = 5
-b = 20
+# def get_strategy(num, current_state_id: int = 0, action: callable = multiply_by_two):
+#     temp = action(num)
+#
+#     if temp < b and temp != 0:
+#         states.append(State(temp, action.__name__))
+#         return get_strategy(temp, current_state_id + 1, multiply_by_two)
+#     elif temp == b:
+#         states.append(State(temp, action.__name__))
+#         final_states = states[1:]
+#         return [alias[state.action_name] for state in final_states]
+#     else:
+#         prev_id = current_state_id - 1
+#         if prev_id > 0:
+#             return get_strategy(states[prev_id].num, prev_id, add_one_to_end)
+#         else:
+#             return False
+
+def get_strategy(num: int, current_state_id: int = 0, action: callable = multiply_by_two):
+    if num == b:
+        return [alias['nothing_to_do']]
+    else:
+        if num == 0:
+            action = add_one_to_end
+        temp = action(num)
+        if temp < b:
+            states.append(State(temp, action.__name__))
+            return get_strategy(temp, current_state_id + 1, action)
+        elif temp == b:
+            states.append(State(temp, action.__name__))
+            final_states = states[1:]
+            return [[alias[state.action_name], state.num] for state in final_states]
+        else:
+            prev_id = current_state_id - 1
+            if prev_id > 0:
+                action = add_one_to_end if action == multiply_by_two else multiply_by_two
+                return get_strategy(states[prev_id].num, prev_id, action)
+            else:
+                return False
+
+
+while True:
+    try:
+        a = int(input('Изначальное число: '))
+        b = int(input('Желаемое число: '))
+        break
+    except ValueError:
+        print('Пожалуйста, введите целые числа')
 
 states = [State(a, None)]
-
-
-def get_strategy(num, current_state_id: int = 0, action: callable = multiply_by_two):
-    temp = action(num)
-
-    if temp < b:
-        states.append(State(temp, action.__name__))
-        return get_strategy(temp, current_state_id + 1, multiply_by_two)
-    elif temp == b:
-        states.append(State(temp, action.__name__))
-        final_states = states[1:]
-        return [state.action_name for state in final_states]
-    else:
-        prev_id = current_state_id - 1
-        if prev_id > 0:
-            return get_strategy(states[prev_id].num, prev_id, add_one_to_end)
-        else:
-            return False
+alias = {
+    'multiply_by_two': 'Умножить на два',
+    'add_one_to_end': 'Приписать единицу в конец',
+    'nothing_to_do': 'Ничего делать не нужно, изначальное и желаемое числа равны'
+}
 
 
 res = get_strategy(a)
-print(res)
+if isinstance(res, list):
+    print('Победная стратегия найдена:')
+    for key, instruction in enumerate(res):
+        print(f'{key + 1}) {instruction}')
+else:
+    print('Победная стратегия отсутсвует')
