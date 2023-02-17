44124f697e54adaefbf22618440a76357272b31e
velzevelt <novaraptor13@gmail.com>
2023-02-17 18:54:25 +0300

    upd

--- t-shirts/t-shirt.py
+++ t-shirts/t-shirt.py
@@ -3,13 +3,25 @@ import random
 
 class Student:
     sizes = ('S', 'M', 'L', 'XL', 'XXL', 'XXXL')
+    sure_chance = .75  # шанс на то, что студент знает свой размер
 
     def __init__(self):
         self.desired_size = self.get_random_size()
 
     def get_random_size(self):
-        return random.choice(sizes)
+        if len(self.sizes) > 2:
+            sure = random.random()
+            if self.sure_chance > sure:
+                return random.choice(self.sizes)
+            else:
+                first = random.choice(self.sizes)
+                second = random.choice(self.sizes)
+                while second == first:
+                    second = random.choice(self.sizes)
+
+                return [first, second]
+        else:
+            raise IndexError("Init error need at least 2 to process")
 
 
 students = [Student() for i in range(5)]
-print(students)
