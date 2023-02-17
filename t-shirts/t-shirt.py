e1c31b22e828d5598f91fc11609d285736152f52
velzevelt <novaraptor13@gmail.com>
2023-02-17 20:21:32 +0300

    upd

--- t-shirts/t-shirt.py
+++ t-shirts/t-shirt.py
@@ -1,70 +1,69 @@
 import random
+from functools import lru_cache
+
+def main():
+    def get_random_qty(max_qty: int = 10):
+        return random.randrange(0, max_qty)
 
 
-class Size:
-    def __init__(self, size_alias: str, quantity: int = 0):
-        self.size_alias = size_alias
-        self.quantity = quantity
+    sizes = {
+        'S': get_random_qty(3),
+        'M': get_random_qty(3),
+        'L': get_random_qty(3),
+        'XL': get_random_qty(3),
+        'XXL': get_random_qty(3),
+        'XXXL': get_random_qty(3),
+
+    }
 
 
-def get_random_qty(max_qty: int = 10):
-    return random.randrange(0, max_qty)
+    class Student:
+        sure_chance = .75  # шанс на то, что студент знает свой размер
 
+        def __init__(self):
+            self.desired_size = self.get_random_size()
 
-sizes = (
-    Size('S', get_random_qty(32)),
-    Size('M', get_random_qty(32)),
-    Size('L', get_random_qty(32)),
-    Size('XL', get_random_qty(32)),
-    Size('XXL', get_random_qty(32)),
-    Size('XXXL', get_random_qty(32)),
-
-)
-
-class Student:
-    sure_chance = .75  # шанс на то, что студент знает свой размер
-
-    def __init__(self):
-        self.desired_size = self.get_random_size()
-
-    def get_random_size(self):
-        if len(sizes) > 2:
-            sure = random.random()
-            if self.sure_chance > sure:
-                return random.choice(sizes).size_alias
-            else:
-                max_bound = len(sizes) - 1
-                rand_id = random.randrange(0, max_bound)
-                first = sizes[rand_id]
-
-                if rand_id != max_bound:
-                    second = sizes[rand_id + 1]
+        @lru_cache(maxsize=32)
+        def get_random_size(self):
+            if len(sizes) > 2:
+                sure = random.random()
+                if self.sure_chance > sure:
+                    keys = list(sizes.keys())
+                    return random.choice(keys)
                 else:
-                    second = sizes[rand_id - 1]
+                    keys = list(sizes.keys())
+                    max_bound = len(keys) - 1
+                    rand_id = random.randrange(0, max_bound)
+                    first = keys[rand_id]
+                    if rand_id != max_bound:
+                        second = keys[rand_id + 1]
+                    else:
+                        second = keys[rand_id - 1]
 
-                return first.size_alias, second.size_alias
-        else:
-            raise IndexError("Init error need at least 2 to process")
-
-
-needed_sizes = {}
-for i in range(15):
-    student = Student()
-
-    if type(student.desired_size) is tuple:
-        for val in student.desired_size:
-            if val in needed_sizes:
-                needed_sizes[val] += 1
+                    return first, second
             else:
-                needed_sizes[val] = 1
-    else:
-        if student.desired_size in needed_sizes:
-            needed_sizes[student.desired_size] += 1
+                raise IndexError("Init error need at least 2 to process")
+
+    needed_sizes = {}
+    for i in range(15):
+        student = Student()
+        if type(student.desired_size) is tuple:
+            for val in student.desired_size:
+                if val in needed_sizes:
+                    needed_sizes[val] += 1
+                else:
+                    needed_sizes[val] = 1
         else:
-            needed_sizes[student.desired_size] = 1
+            if student.desired_size in needed_sizes:
+                needed_sizes[student.desired_size] += 1
+            else:
+                needed_sizes[student.desired_size] = 1
 
-print(needed_sizes)
+    for val in needed_sizes:
+        if val in sizes:
+            if needed_sizes[val] > sizes[val]:
+                print('Футболок не хватает')
+                break
 
-# for val in needed_sizes:
-#     temp = Size(val, needed_sizes[val])
-#     if temp.size_alias == sizes[val]
+    print('Футболок хватит всем')
+    
