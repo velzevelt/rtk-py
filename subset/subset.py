5238ed696f9597198e45383146d4083167181d64
Class_Student <Class_Student@rtk.local>
2023-03-29 10:46:57 +0300

    upd

--- subset/subset.py
+++ subset/subset.py
@@ -9,35 +9,24 @@ def main():
     ar = [i for i in range(100) if i % 2 != 0]
     print(ar)
 
-    num = 15
-    # while True:
-    #     try:
-    #         num = int(input("Введите желаемое число "))
-    #         break
-    #     except ValueError:
-    #         pass
-    #     except KeyboardInterrupt:
-    #         return
+    # num = 8
+    while True:
+        try:
+            num = int(input("Введите желаемое число "))
+            break
+        except ValueError:
+            pass
+        except KeyboardInterrupt:
+            return
 
-    closest = 0
-    last = 0
     for key, val in enumerate(ar):
         if key == 0:
             continue
-
         prev = ar[key - 1]
-        step = prev + val
-        if step == num:
+        if val + prev == num:
             print(prev, val)
             break
-        elif step > num:
-            break
-        else:
-            last = val
-            closest = step
-    t = num - closest
-    if t in ar:
-        print(t, last)
+
     else:
         print("Пара не найдена")
 
